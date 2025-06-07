fd14586ba61c602743e380c62f7ca4dc6c1c6024
Edvinas Buzas <edvix08@gmail.com>
2025-06-07 21:49:56 +0200

    BIG COMMIT base structure and basic ticket domain

---
+++ src/app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,40 @@
+import { initialTickets } from "@/data";
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ticketsPath } from "@/paths";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
+
+type TicketPageProps = {
+  params: Promise<{
+    ticketId: string;
+  }>;
+};
+
+const TicketPage = async ({ params }: TicketPageProps) => {
+  const { ticketId } = await params;
+  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
+  if (!ticket) {
+    return (
+      <div className="flex flex-1">
+        <Placeholder
+          label="Ticket not found"
+          button={
+            <Button asChild variant="outline">
+              <Link href={ticketsPath()}>Go to Tickets</Link>
+            </Button>
+          }
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center animate-fade-from-top">
+      <TicketItem ticket={ticket} isDetail />
+    </div>
+  );
+};
+
+export default TicketPage;
---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import { homePath, ticketsPath } from "@/paths";
+import { LucideKanban } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+const Header = () => {
+  return (
+    <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 backdrop-blur w-full fixed left-0 right-0 top-0 z-20 flex justify-between py-2.5 px-5 border-b">
+      <div>
+        <Link
+          href={homePath()}
+          className={buttonVariants({ variant: "ghost" })}
+        >
+          <LucideKanban />
+          <h1 className="text-lg font-semibold">TicketBounty</h1>
+        </Link>
+      </div>
+      <div>
+        <Link
+          href={ticketsPath()}
+          className={buttonVariants({ variant: "default" })}
+        >
+          Tickets
+        </Link>
+      </div>
+    </nav>
+  );
+};
+export { Header };
