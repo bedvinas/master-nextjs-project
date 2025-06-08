3f8d3923611f9debe7014dce815a2928b121f3db
Edvinas Buzas <edvix08@gmail.com>
2025-06-08 17:12:18 +0200

    added theme provider and switcher

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,12 @@ import { homePath, ticketsPath } from "@/paths";
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 
 const Header = () => {
   return (
     <nav className="supports-backdrop-blur:bg-background/60 bg-background/95 backdrop-blur w-full fixed left-0 right-0 top-0 z-20 flex justify-between py-2.5 px-5 border-b">
-      <div>
+      <div className="flex align-items gap-x-2">
         <Link
           href={homePath()}
           className={buttonVariants({ variant: "ghost" })}
@@ -15,7 +16,8 @@ const Header = () => {
           <h1 className="text-lg font-semibold">TicketBounty</h1>
         </Link>
       </div>
-      <div>
+      <div className="flex align-items gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({ variant: "default" })}
