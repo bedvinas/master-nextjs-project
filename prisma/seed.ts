3aac188ac603796ccae648e420944bed9119f299
Edvinas Buzas <edvix08@gmail.com>
2025-06-12 18:43:03 +0200

    prisma schema, ticket model and seed

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+const tickets = [
+  {
+    title: "Ticket 1",
+    content: "This is the first ticket from the database",
+    status: "DONE" as const,
+  },
+  {
+    title: "Ticket 2",
+    content: "This is the second ticket from the database",
+    status: "OPEN" as const,
+  },
+  {
+    title: "Ticket 3",
+    content: "This is the third ticket from the database",
+    status: "IN_PROGRESS" as const,
+  },
+];
+
+const seed = async () => {
+  // for-of
+  //   for (const ticket of tickets) {
+  //     await prisma.ticket.create({
+  //       data: ticket,
+  //     });
+  //   }
+
+  // All promises at once
+  //   const promises = tickets.map((ticket) => {
+  //     prisma.ticket.create({
+  //       data: ticket,
+  //     });
+  //   });
+  //   await Promise.all(promises);
+
+  // before seeding, delete old data:
+  console.time("DB Seed");
+  await prisma.ticket.deleteMany({});
+
+  // built-in prisma method
+  await prisma.ticket.createMany({
+    data: tickets,
+  });
+  console.timeEnd("DB Seed");
+};
+
+seed();
