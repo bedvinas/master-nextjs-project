76c6ecd7c18d05c8a3bcfbd9d264724750b8c8a7
Edvinas Buzas <edvix08@gmail.com>
2025-07-06 19:34:15 +0200

    redirect-toast moved to template in order to appear application wide

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,7 +6,6 @@ import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { CardCompact } from "@/components/card-compact";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
-import { RedirectToast } from "@/components/redirect-toast";
 
 // force dynamic rendering to avoid full route cache:
 // export const dynamic = "force-dynamic";
@@ -28,18 +27,17 @@ const TicketsPage = () => {
   // }, []);
 
   return (
-    <>
-      <div className="flex-1 flex flex-col gap-y-8">
-        <Heading title="Tickets" description="All your tickets at one place" />
-        {/* Configuration over Composition: */}
-        <CardCompact
-          title="Create Ticket"
-          description="A new ticket will be created"
-          className="w-full max-w-[420px] self-center"
-          content={<TicketUpsertForm />}
-        />
-        {/* Composition over Configuration: */}
-        {/* <Card className="w-full max-w-[420px] self-center">
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Tickets" description="All your tickets at one place" />
+      {/* Configuration over Composition: */}
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketUpsertForm />}
+      />
+      {/* Composition over Configuration: */}
+      {/* <Card className="w-full max-w-[420px] self-center">
         <CardHeader>
           <CardTitle>Create Ticket</CardTitle>
           <CardDescription>A new ticket will be created</CardDescription>
@@ -48,12 +46,10 @@ const TicketsPage = () => {
           <TicketCreateForm />
         </CardContent>
       </Card> */}
-        <Suspense fallback={<Spinner />}>
-          <TicketList />
-        </Suspense>
-      </div>
-      <RedirectToast />
-    </>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
+    </div>
   );
 };
 
--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -3,8 +3,12 @@
 import { consumeCookieByKey } from "@/actions/cookies";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { usePathname } from "next/navigation";
 
 const RedirectToast = () => {
+  // Alternatively instead of using pathname as dependency in useEffect, we can use template.tsx which rerenders on navigation by default
+  // But template.tsx has issues (06.07.2025) and does not rerender as on nested routes
+  const pathname = usePathname();
   useEffect(() => {
     const showCookieToast = async () => {
       const message = await consumeCookieByKey("toast");
@@ -14,7 +18,7 @@ const RedirectToast = () => {
     };
 
     showCookieToast();
-  }, []);
+  }, [pathname]);
 
   return null;
 };
